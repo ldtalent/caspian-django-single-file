29b7a8287e043b498aaba5af52e7b5c0d8abad6e
Immanuel Kolapo <immanuelcaspian@gmail.com>
2019-12-12 13:43:45 +0100

    Added mandatory configurations in settings.configure() - MIDDLEWARE, INSTALLED_APPS, TEMPLATES. Also added django.setup()

--- blog/blog.py
+++ blog/blog.py
@@ -1,32 +1,73 @@
+import os
 import sys
+from django import setup
+from django.contrib import admin
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 from django.urls import path
 from decouple import config, Csv
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 settings.configure(
 	DEBUG=config('DEBUG', default=False, cast=bool),
 	SECRET_KEY=config('SECRET_KEY'),
 	ALLOWED_HOSTS=config('ALLOWED_HOSTS', cast=Csv()),
 	ROOT_URLCONF = __name__,
-	MIDDLEWARE_CLASSES = [
+	INSTALLED_APPS = [
+		'django.contrib.admin',
+		'django.contrib.auth',
+		'django.contrib.contenttypes',
+		'django.contrib.sessions',
+		'django.contrib.messages',
+		'django.contrib.staticfiles',
+	],
+	MIDDLEWARE = [
+		'django.middleware.security.SecurityMiddleware',
+    	'django.contrib.sessions.middleware.SessionMiddleware',
 		'django.middleware.common.CommonMiddleware',
 		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.contrib.auth.middleware.AuthenticationMiddleware',
+		'django.contrib.messages.middleware.MessageMiddleware',
 		'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	],
+	TEMPLATES = [
+    	{
+        	'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        	'DIRS': [
+            	os.path.join(BASE_DIR, 'templates'),
+        	],
+        	'APP_DIRS': True,
+			'OPTIONS': {
+				'context_processors': [
+					'django.template.context_processors.debug',
+					'django.template.context_processors.request',
+					'django.contrib.auth.context_processors.auth',
+					'django.contrib.messages.context_processors.messages',
+				],
+			},
+		},
 	],
 	DATABASES={
 		'default': {
 			'ENGINE': 'django.db.backends.sqlite3',
 			'NAME': ('db.sqlite3'),
 		}
-	}
+	},
+	STATIC_URL = '/static/',
+	STATICFILES_DIR = [
+    	os.path.join(BASE_DIR, 'static'),
+	]
 )
+
+setup()
 
 def index(request):
 	return HttpResponse("<h1>Hello, this is a minimal project setup. Configure as you please!</h1>")
 
 urlpatterns = [
+	path('admin/', admin.site.urls),
 	path('', index, name='index'),
 ]
 
